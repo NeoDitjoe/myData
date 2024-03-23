18dd47a216b758b181b0f055c67b19622a35c2f9
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-03-23 19:07:29 +0200

    display pie chart

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -5,6 +5,29 @@ fetch('../../../util/pieChart.json')
   .then(res => res.json())
   .then(data => numInstalls(data))
 
+import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+    name: 'PieChart',
+    components: { Pie },
+    data() {
+        return {
+            chartData: {
+                labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+                datasets: [
+                    {
+                        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+                        data: [40, 20, 80, 10]
+                    }
+                ]
+            },
+            chartOptions: {
+                responsive: true
+            }
+        }
+    }}
   
 
 </script>
@@ -12,7 +35,7 @@ fetch('../../../util/pieChart.json')
 <template>
   <div class="greetings">
     <h1 class="green">Home</h1>
-    
+    <Pie id="my-chart-id" :options="chartOptions" :data="chartData" />
   </div>
 </template>
 
