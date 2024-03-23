533360a5bbda5b346ef2af145225f50ed85bd4dd
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-03-24 00:05:18 +0200

    table style

--- src/components/users-table/Users-table.vue
+++ src/components/users-table/Users-table.vue
@@ -45,20 +45,26 @@ export default {
 
   <table style="border: 1px solid black;">
     <tr>
-      <th>Name</th>
-      <th>Surname</th>
-      <th>Designation</th>
-      <th>Department</th>
+      <th class="th">No.</th>
+      <th class="th">Name</th>
+      <th class="th">Surname</th>
+      <th class="th">Designation</th>
+      <th class="th">Department</th>
     </tr>
 
     <tbody>
       <tr v-for="(user, index) in userData" :key = "user.name">
-        <td style="border: 1px solid black;">{{(index + 1) + ' - '+ user.name }}</td>
-        <td style="border: 1px solid black;">{{ user.surname }}</td>
-        <td style="border: 1px solid black;">{{ user.designation }}</td>
-        <td style="border: 1px solid black;">{{ user.department }}</td>
+        <td class="index">{{ index + 1 }}</td>
+        <td class="td">{{user.name }}</td>
+        <td class="td">{{ user.surname }}</td>
+        <td class="td">{{ user.designation }}</td>
+        <td class="td">{{ user.department }}</td>
       </tr>
     </tbody>
 
   </table>
 </template>
+
+<style>
+@import './user-table.css';
+</style>
