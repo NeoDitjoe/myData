e5dda8165242293a4190e06c5701e8c3bd0c0343
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-03-24 22:08:35 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
     </div>
     
     <div class="menu">
-      <router-link @click="highlightLink" :class="{ active: this.$router.currentRoute.value.path === '/' }" to="/">Installs_Pie_Graph</router-link>
+      <router-link @click="highlightLink" :class="{ active: this.$router.currentRoute.value.path === '/' }" to="/">Installs_Pie_Chart</router-link>
       <router-link @click="highlightLink" :class="{ active: this.$router.currentRoute.value.path === '/bar-graph' }" to="/bar-graph">Installs_Bar_Graph</router-link>
       <router-link @click="highlightLink" :class="{ active: this.$router.currentRoute.value.path === '/users-table' }" to="/users-table?page=0">Users_Table</router-link>
     </div>
