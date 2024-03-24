fa11950c2ce4943f2972ff0640edb9b4b593b070
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-03-24 14:46:41 +0200

    on active page

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,24 @@
-<script setup>
+<script>
+
+export default {
+
+  data(){
+    return {
+      currentPage: null
+    }
+  },
+
+  created() {
+    this.currentPage = this.$router.currentRoute.value.path;
+  },
+
+  methods: {
+    highlightLink(){
+      console.log(this.$router.currentRoute.value.path)
+      this.currentPage = this.$router.currentRoute.value.path
+    }
+  }
+}
 
 </script>
 
@@ -6,13 +26,12 @@
   <nav class="nav">
     <div>
       <h1>Logo</h1>
-      <!-- <router-link to="/about">Home</router-link> -->
     </div>
     
     <div>
-      <router-link to="/">Installs Bar Graph</router-link>
-      <router-link to="/bar-graph">Installs Bar Graph</router-link>
-      <router-link to="/users-table?page=0">Users Table</router-link>
+      <router-link @click="highlightLink" :class="{ active: this.$router.currentRoute.value.path === '/' }" to="/">Installs Bar Graph</router-link>
+      <router-link @click="highlightLink" :class="{ active: this.$router.currentRoute.value.path === '/bar-graph' }" to="/bar-graph">Installs Bar Graph</router-link>
+      <router-link @click="highlightLink" :class="{ active: this.$router.currentRoute.value.path === '/users-table' }" to="/users-table?page=0">Users Table</router-link>
     </div>
   </nav>
 
