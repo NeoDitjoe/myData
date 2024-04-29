476e29c24c16620d5d388d1a224ca0839b184b7f
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-29 18:44:29 +0200

    set up server

---
+++ src/util/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+const app = express()
+app.use(cors)
+app.use(bodyParser.json())
+
+const port = 3000
+
+
+
+app.listen(port, () => {
+  console.log('connected to server')
+})
