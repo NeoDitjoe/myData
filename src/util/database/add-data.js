0b162a1c67aa195c1bf297158f572536114acaf2
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-29 20:42:37 +0200

    added data to mongo

--- src/util/database/add-data.js
+++ src/util/database/add-data.js
@@ -5,7 +5,7 @@ export async function addData(){
 
   const db = client.db('data')
 
-  await db.collection('Monthly-installs')
+  await db.collection('monthly-installs')
     .insertMany([
       {
         "month": "2023-08",
