855c8d2e1fc00b36ed32818a89f853a4d1d2cbe1
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-29 19:33:25 +0200

    install nodemon

--- src/util/server.js
+++ src/util/server.js
@@ -3,13 +3,15 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 
 const app = express()
-app.use(cors)
+app.use(cors())
 app.use(bodyParser.json())
 
-const port = 3000
+const port = process.env.PORT || 5100
 
-
+app.use('/api/hello-world', (req, res) => {
+  res.send('Hello World')
+})
 
 app.listen(port, () => {
-  console.log('connected to server')
+  console.log(`connected to server ${port}`)
 })
